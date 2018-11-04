a941cabd1072d2ab7c8ba64670e6882402ac3419
saurabhomer258 <saurabhomer258@gmail.com>
2018-11-04 11:03:05 +0530

    registration page

--- login/controllers/authhander.js
+++ login/controllers/authhander.js
@@ -12,21 +12,33 @@ var con = mysql.createConnection({
 module.exports = function(app)
 {
     var bodyParser = require('body-parser');
-    var data =[{item: 'get milk'},{item: 'walk dog'}];
+
     var urlencodedParser = bodyParser.urlencoded({ extended: false });
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+      var sql = "CREATE TABLE if not exists student (name VARCHAR(255), mail VARCHAR(255),pass VARCHAR(255))";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+      });
+    });
 
      app.post('/authhander', urlencodedParser, function (req, res)
         {
-        var sql = 'SELECT * FROM customers WHERE name = ' + mysql.escape(req.body.name);
+        var sql = 'SELECT * FROM student where name= '+ mysql.escape(req.body.name) + 'AND pass=' + mysql.escape(req.body.pass)  ;
+
         con.query(sql, function (err, result) {
+        console.log(result);
         if(result.length >0)
         {
 
-                console.log("yes");
+               res.render('home');
         }
         else
         {
-            console.log("No");
+             res.render('index');
+
             }
         });
 
